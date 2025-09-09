500535dcebe1c734ec75e61020eb0524deda3381
tanmay-cdac <tanmay.sawant.cmaug25@gmail.com>
2025-09-09 11:08:12 +0530

    Delete Problem4.java

--- Problem4.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Problem4 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println((n*(n+1))/2);
-        sc.close();
-    }
-}
